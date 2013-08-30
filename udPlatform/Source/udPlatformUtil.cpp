09e2e26eedcc1d5a10b880eb9b9e3ed1935db22c
Dave Pevreal <dpevreal@euclideon.com>
2013-08-30 05:33:51 +0000

    Work in progress, with export/import seeming to be correct leaving the renderer as the having an issue
    git-svn-id: https://bne-fs-pf-001.euclideon.local/svn/Dev/trunk/src@3 f54097bf-93f5-7042-b87d-fd266665e7ff

---
+++ udPlatform/Source/udPlatformUtil.cpp
@@ -0,0 +1,29 @@
+#include "udPlatformUtil.h"
+#include <string.h>
+
+
+int AddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char *addString)
+{
+  int offset = 0;
+  int addStrLen = (int)strlen(addString); // Length of string to be added
+
+  while (offset < stringTableLength)
+  {
+    int curStrLen = (int)strlen(stringTable + offset);
+    if (offset + curStrLen > stringTableLength)
+      break; // A catch-all in case something's gone wrong
+    if (curStrLen >= addStrLen && strcmp(stringTable + offset + curStrLen - addStrLen, addString) == 0)
+      return offset + curStrLen - addStrLen; // Found a match
+    else
+      offset += curStrLen + 1;
+  }
+  int newLength = offset + addStrLen + 1;
+  char *newCache = (char*)realloc(stringTable, newLength);
+  if (!newCache)
+    return -1; // A nasty case where memory allocation has failed
+  strcpy(newCache + offset, addString);
+  stringTable = newCache;
+  stringTableLength = offset + addStrLen + 1;
+  return offset;
+}
+
