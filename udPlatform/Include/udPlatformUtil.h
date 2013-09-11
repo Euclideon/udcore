e0207bdb989457719016a2431863178665739af1
Dave Pevreal <dpevreal@euclideon.com>
2013-09-11 04:18:32 +0000

    Added udStrcpy etc functions as strcpy_s etc are MS-specific Create udFilename class to wrap up filename utility functions
    git-svn-id: https://bne-fs-pf-001.euclideon.local/svn/Dev/trunk/src@16 f54097bf-93f5-7042-b87d-fd266665e7ff

--- udPlatform/Include/udPlatformUtil.h
+++ udPlatform/Include/udPlatformUtil.h
@@ -3,8 +3,28 @@
 
 #include "udPlatform.h"
 
+// *********************************************************************
+// Outputs a string to debug console
+// *********************************************************************
 void udDebugPrintf(const char *format, ...);
 
+// *********************************************************************
+// Some string functions that are safe, 
+//  - on success all return the length of the result in characters 
+//    (including null) which is always greater than zero
+//  - on overflow a zero returned, also note that
+//    a function designed to overwrite the destination (eg udStrcpy) 
+//    will clear the destination. Functions designed to work
+//    with a valid destination (eg udStrcat) will leave the
+//    the destination unaltered.
+// *********************************************************************
+size_t udStrcpy(char *dest, size_t destLen, const char *src);
+size_t udStrncpy(char *dest, size_t destLen, const char *src, size_t maxChars);
+size_t udStrcat(char *dest, size_t destLen, const char *src);
+
+// *********************************************************************
+// Helper for growing an array as required
+// *********************************************************************
 template<typename T>
 bool udSizeArray(T *&ptr, uint32_t &currentLength, uint32_t requiredLength, int32_t allocationMultiples = 0)
 {
@@ -25,15 +45,77 @@ bool udSizeArray(T *&ptr, uint32_t &currentLength, uint32_t requiredLength, int3
   return true;
 }
 
-/// \brief Count the number of bits set in a 8 bit number
+// *********************************************************************
+// Count the number of bits set in a 8 bit number
+// *********************************************************************
 inline uint32_t udCountBits(uint8_t a_number)
 {
   static const uint8_t bits[256] = { 0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,3,4,4,5,4,5,5,6,4,5,5,6,5,6,6,7,4,5,5,6,5,6,6,7,5,6,6,7,6,7,7,8 };
   return bits[a_number];
 }
 
+// *********************************************************************
+// Add a string to a dynamic table of unique strings. 
+// Initialise stringTable to NULL and stringTableLength to 0, 
+// and table will be reallocated as necessary
+// *********************************************************************
+int udAddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char *addString);
 
-// Add a string to a dynamic table of unique strings. Initialise stringTable to NULL and stringTableLength to 0, and table will be reallocated as necessary
-int AddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char *addString);
+
+// *********************************************************************
+// A helper class for dealing with filenames, no memory allocation
+// *********************************************************************
+class udFilename
+{
+public:
+  // Construct either empty as default or from a path
+  // No destructor (or memory allocation) to keep the object simple and copyable
+  udFilename() { SetFromFullPath(nullptr); }
+  udFilename(const char *path) { SetFromFullPath(path); }
+
+  enum { MaxPath = 260 };
+
+  //Cast operator for convenience
+  operator const char *() { return m_path; }
+
+  //
+  // Set methods: (set all or part of the internal fully pathed filename, return false if path too long)
+  //    FromFullPath      - Set all components from a fully pathed filename
+  //    Folder            - Set the folder, retaining the existing filename and extension
+  //    SetFilenameNoExt  - Set the filename without extension, retaining the existing folder and extension
+  //    SetExtension      - Set the extension, retaining the folder and filename
+  //
+  bool SetFromFullPath(const char *fullPath);
+  bool SetFolder(const char *folder);
+  bool SetFilenameNoExt(const char *filenameOnlyComponent);
+  bool SetFilenameWithExt(const char *filenameWithExtension);
+  bool SetExtension(const char *extComponent);
+
+  //
+  // Get methods: (all return a const pointer to the internal fully pathed filename)
+  //    GetPath             - Get the complete path (eg pass to fopen)
+  //    GetFilenameWithExt  - Get the filename and extension, without the path
+  //    GetExt              - Get just the extension (starting with the dot)
+  const char *GetPath()                 { return m_path; }
+  const char *GetFilenameWithExt()      { return m_path + m_filenameIndex; }
+  const char *GetExt()                  { return m_path + m_extensionIndex; }
+
+  //
+  // Extract methods: (take portions from within the full path to a user supplied buffer, returning size required)
+  //    ExtractFolder       - Extract the folder, including the trailing slash
+  //    ExtractFilenameOnly - Extract the filename without extension
+  //
+  int ExtractFolder(char *folder, int folderLen);
+  int ExtractFilenameOnly(char *filename, int filenameLen);
+
+  // Temporary function to output debug info until unit tests are done to prove reliability
+  void Debug();
+
+protected:
+  void CalculateIndices();
+  int m_filenameIndex;      // Index to starting character of filename
+  int m_extensionIndex;     // Index to starting character of extension
+  char m_path[260];         // Buffer for the path, set to 260 characters
+};
 
 #endif // UDPLATFORM_UTIL_H
--- udPlatform/Source/udPlatformUtil.cpp
+++ udPlatform/Source/udPlatformUtil.cpp
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 
 
+// *********************************************************************
 void udDebugPrintf(const char *format, ...)
 {
   va_list args;
@@ -19,8 +20,60 @@ void udDebugPrintf(const char *format, ...)
 #endif
 }
 
+// *********************************************************************
+size_t udStrcpy(char *dest, size_t destLen, const char *src)
+{
+  if (dest == NULL)
+    return 0;
+  if (src == NULL) // Special case, handle a NULL source as an empty string
+    src = "";
+  size_t srcChars = strlen(src);
+  if ((srcChars + 1) > destLen)
+  {
+    *dest = 0;
+    return 0;
+  }
+  memcpy(dest, src, srcChars + 1);
+  return srcChars + 1;
+}
 
-int AddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char *addString)
+// *********************************************************************
+size_t udStrncpy(char *dest, size_t destLen, const char *src, size_t maxChars)
+{
+  if (dest == NULL)
+    return 0;
+  if (src == NULL) // Special case, handle a NULL source as an empty string
+    src = "";
+  size_t srcChars = strlen(src);
+  if (srcChars > maxChars)
+    srcChars = maxChars;
+  if ((srcChars + 1) > destLen)
+  {
+    *dest = 0;
+    return 0;
+  }
+  memcpy(dest, src, srcChars); // Use crt strcpy as it's likely to be very fast
+  dest[srcChars] = 0; // Nul terminate
+  return srcChars + 1;
+}
+
+// *********************************************************************
+size_t udStrcat(char *dest, size_t destLen, const char *src)
+{
+  if (dest == NULL)
+    return 0;
+  if (src == NULL) // Special case, handle a NULL source as an empty string
+    src = "";
+  size_t destChars = strlen(dest); // Note: Not including terminator
+  size_t srcChars = strlen(src);
+  if ((destChars + srcChars + 1) > destLen)
+    return 0;
+  strcat(dest, src);
+  return destChars + srcChars + 1;
+}
+
+// *********************************************************************
+int udAddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char *addString)
 {
   uint32_t offset = 0;
   int addStrLen = (int)strlen(addString); // Length of string to be added
@@ -45,3 +98,141 @@ int AddToStringTable(char *&stringTable, uint32_t &stringTableLength, const char
   return offset;
 }
 
+// *********************************************************************
+bool udFilename::SetFromFullPath(const char *fullPath)
+{
+  *m_path = 0;
+  m_filenameIndex = 0;
+  m_extensionIndex = 0;
+  if (fullPath)
+  {
+    if (!udStrcpy(m_path, sizeof(m_path), fullPath))
+      return false;
+    CalculateIndices();
+  }
+  return true;
+}
+
+// *********************************************************************
+bool udFilename::SetFolder(const char *folder)
+{
+  char newPath[MaxPath];
+  size_t i = udStrcpy(newPath, sizeof(m_path), folder);
+  if (!i)
+    return false;
+
+  // If the m_path doesn't have a trailing seperator, look for one so we can
+  // append one already being used. That is c:\\m_path\\ or c:/m_path/
+  if (i > 2 && newPath[i-2] != '/' && newPath[i-2] != '\\' && newPath[i-2] != ':')
+  {
+    for (--i; i > 0; --i)
+    {
+      if (newPath[i-1] == '\\' || newPath[i-1] == ':')
+      {
+        udStrcat(newPath, sizeof(newPath), "\\");
+        break;
+      }
+      if (newPath[i-1] == '/')
+      {
+        udStrcat(newPath, sizeof(newPath), "/");
+        break;
+      }
+    }
+    // Nothing was found so add a /. TODO: Get correct separator from system
+    if (i == 0)
+      udStrcat(newPath, sizeof(newPath), "/");
+  }
+
+  if (!udStrcat(newPath, sizeof(newPath), GetFilenameWithExt()))
+    return false;
+  return SetFromFullPath(newPath);
+}
+
+// *********************************************************************
+bool udFilename::SetFilenameNoExt(const char *filenameOnlyComponent)
+{
+  char newPath[MaxPath];
+
+  ExtractFolder(newPath, sizeof(newPath));
+  if (!udStrcat(newPath, sizeof(newPath), filenameOnlyComponent))
+    return false;
+  if (!udStrcat(newPath, sizeof(newPath), GetExt()))
+    return false;
+  return SetFromFullPath(newPath);
+}
+
+// *********************************************************************
+bool udFilename::SetFilenameWithExt(const char *filenameWithExtension)
+{
+  char newPath[MaxPath];
+
+  ExtractFolder(newPath, sizeof(newPath));
+  if (!udStrcat(newPath, sizeof(newPath), filenameWithExtension))
+    return false;
+  return SetFromFullPath(newPath);
+}
+
+// *********************************************************************
+bool udFilename::SetExtension(const char *extComponent)
+{
+  char newPath[MaxPath];
+
+  if (!udStrcpy(newPath, sizeof(newPath), m_path))
+    return false;
+  newPath[m_extensionIndex] = 0; // Truncate the extension
+  if (!udStrcat(newPath, sizeof(newPath), extComponent))
+    return false;
+  return SetFromFullPath(newPath);
+}
+
+// *********************************************************************
+int udFilename::ExtractFolder(char *folder, int folderLen)
+{
+  int folderChars = m_filenameIndex;
+  if (folder != NULL)
+    udStrncpy(folder, folderLen, m_path, folderChars);
+  return folderChars + 1;
+}
+
+// *********************************************************************
+int udFilename::ExtractFilenameOnly(char *filename, int filenameLen)
+{
+  int filenameChars = m_extensionIndex - m_filenameIndex;
+  if (filename != NULL)
+  {
+    udStrncpy(filename, filenameLen, m_path + m_filenameIndex, filenameChars);
+  }
+  return filenameChars + 1;
+}
+
+// ---------------------------------------------------------------------
+void udFilename::CalculateIndices()
+{
+  int len = (int)strlen(m_path);
+  // Set filename and extension indices to null terminator as a sentinal
+  m_filenameIndex = -1;
+  m_extensionIndex = len; // If no extension, point extension to nul terminator
+
+  for (--len; len > 0 && (m_filenameIndex == -1 || m_extensionIndex == -1); --len)
+  {
+    if (m_path[m_extensionIndex] == 0 && m_path[len] == '.') // Last period
+      m_extensionIndex = len;
+    if (m_filenameIndex == -1 && (m_path[len] == '/' || m_path[len] == '\\' || m_path[len] == ':'))
+      m_filenameIndex = len + 1;
+  }
+  // If no path indicators were found the filename is the beginning of the path
+  if (m_filenameIndex == -1)
+    m_filenameIndex = 0;
+}
+
+
+// *********************************************************************
+void udFilename::Debug()
+{
+  char folder[260];
+  char name[50];
+
+  ExtractFolder(folder, sizeof(folder));
+  ExtractFilenameOnly(name, sizeof(name));
+  udDebugPrintf("folder<%s> name<%s> ext<%s> filename<%s> -> %s\n", folder, name, GetExt(), GetFilenameWithExt(), m_path);
+}
