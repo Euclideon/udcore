c04988ed5a797401fef0d7ec93ef3bfd009afb46
Dave Pevreal <dpevreal@euclideon.com>
2014-02-25 01:27:39 +0000

    Moved result codes into their own file
    git-svn-id: https://bne-fs-pf-001.euclideon.local/svn/Dev/trunk/src@118 f54097bf-93f5-7042-b87d-fd266665e7ff

---
+++ udPlatform/Include/udResult.h
@@ -0,0 +1,44 @@
+#ifndef UDRESULT_H
+#define UDRESULT_H
+
+enum udResult
+{
+  udR_Success,
+  udR_Failure_,
+  udR_InternalError,
+  udR_NotInitialized_,
+  udR_InvalidConfiguration,
+  udR_InvalidParameter_,
+  udR_OutstandingReferences,
+  udR_MemoryAllocationFailure,
+  udR_CountExceeded,
+  udR_ObjectNotFound,
+  udR_RenderAlreadyInProgress,
+
+
+  udR_Writer_InitFailure, 
+  udR_Writer_WritePointCloudHeaderFailure, 
+  udR_Writer_WriteBlockHeaderFailure, 
+  udR_Writer_WriteBlockFailure,
+  udR_Writer_ReadBlockFailure,
+  udR_Writer_ReadNextBlockFailure,
+  udR_Writer_WriteBlockInfoArrayFailure,
+  udR_Writer_WriteStringTableFailure,
+  udR_Writer_WriteResourceFailure,
+  udR_Writer_DeinitFailure,
+
+  udR_BlockInfoArrayExhausted, 
+
+  udR_File_OpenFailure,
+  udR_File_CloseFailure,
+  udR_File_NoFileJobsAvailable,
+  udR_File_ReadFailure,
+  udR_File_WriteFailure,
+  udR_File_FailedToAddFileJob,
+  udR_Count,
+
+  udR_ForceInt = 0x7FFFFFFF
+};
+
+
+#endif // UDRESULT_H
