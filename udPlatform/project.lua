99e31c233b1285b35279d3d8009eb99151720ddc
Dave Pevreal <dpevreal@euclideon.com>
2016-10-06 11:13:47 +1000

    Changed library outputs for udPlatform and udPointCloud to match udbin to make switching between source and bin easy

--- udPlatform/project.lua
+++ udPlatform/project.lua
@@ -11,8 +11,12 @@ project ("udPlatform" .. (projectSuffix or ""))
 
 	-- include common stuff
 	dofile "../common-proj.lua"
-	
+
 	filter { "configurations:Release", "system:Windows" }
 		symbols "Off"
-		
+
+	-- for windows, make the output name and location identical to that of udbin
+	filter { "system:Windows" }
+		targetdir "../Lib/%{cfg.system}_%{cfg.shortname}"
+
 	filter {}
