077bbfe8e105f638b4a9c6c33214389e5d60f807
Dave Pevreal <dpevreal@euclideon.com>
2017-06-14 16:23:57 +1000

    Added docs and udPlatform source/.pdb to package process for udBin

--- udPlatform/project.lua
+++ udPlatform/project.lua
@@ -3,7 +3,7 @@ project ("udPlatform" .. (projectSuffix or ""))
 	language "C++"
 	flags { "StaticRuntime", "OmitDefaultLibrary" }
 
-	files { "Source/**", "Include/**" }
+	files { "Source/**", "Include/**", "Docs/**" }
 	files { "project.lua" }
 
 	includedirs { "../3rdParty" }
@@ -13,10 +13,11 @@ project ("udPlatform" .. (projectSuffix or ""))
 	dofile "../common-proj.lua"
 
 	filter { "configurations:Release", "system:Windows" }
-		symbols "Off"
+		symbols "On"
 
 	-- for windows, make the output name and location identical to that of udbin
 	filter { "system:Windows" }
 		targetdir "../Lib/%{cfg.system}_%{cfg.shortname}"
+		symbolspath "$(TargetDir)/$(ProjectName).pdb"
 
 	filter {}
