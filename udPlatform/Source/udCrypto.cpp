a47e230190997da11e3e9c076a86e24aecbdc94e
Samuel Surtees <ssurtees@euclideon.com>
2018-03-06 15:03:30 +1000

    Added option to build with full mbedTLS

--- udPlatform/Source/udCrypto.cpp
+++ udPlatform/Source/udCrypto.cpp
@@ -26,6 +26,7 @@
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
 
+#if !defined(FULL_MBEDTLS)
 extern "C" int mbedtls_snprintf(char * s, size_t n, const char * format, ...)
 {
   va_list ap;
@@ -34,6 +35,7 @@ extern "C" int mbedtls_snprintf(char * s, size_t n, const char * format, ...)
   va_end(ap);
   return len;
 }
+#endif
 
 enum
 {
--- udPlatform/project.lua
+++ udPlatform/project.lua
@@ -1,25 +1,20 @@
+newoption {
+	trigger = "mbedTLS",
+	value = "VALUE",
+	description = "Specify whether you want full or minimal mbedTLS support",
+	allowed = {
+		{ "full", "Full mbedTLS support" },
+		{ "minimal", "Minimal mbedTLS support" },
+	},
+	default = "minimal",
+}
+
 project ("udPlatform" .. (projectSuffix or ""))
 	kind "StaticLib"
 	language "C++"
 	flags { "StaticRuntime", "OmitDefaultLibrary" }
 
 	files { "Source/**", "Include/**", "Docs/**" }
-	files { "../3rdParty/mbedtls/include/mbedtls/config.h" }
-	files { "../3rdParty/mbedtls/library/aes.c", "../3rdParty/mbedtls/include/mbedtls/aes.h" }
-	files { "../3rdParty/mbedtls/library/sha1.c", "../3rdParty/mbedtls/include/mbedtls/sha1.h" }
-	files { "../3rdParty/mbedtls/library/sha256.c", "../3rdParty/mbedtls/include/mbedtls/sha256.h" }
-	files { "../3rdParty/mbedtls/library/sha512.c", "../3rdParty/mbedtls/include/mbedtls/sha512.h" }
-	files { "../3rdParty/mbedtls/library/rsa.c", "../3rdParty/mbedtls/include/mbedtls/rsa.h" }
-	files { "../3rdParty/mbedtls/library/bignum.c", "../3rdParty/mbedtls/include/mbedtls/bignum.h" }
-	files { "../3rdParty/mbedtls/library/entropy.c", "../3rdParty/mbedtls/include/mbedtls/entropy.h" }
-	files { "../3rdParty/mbedtls/library/entropy_poll.c", "../3rdParty/mbedtls/include/mbedtls/entropy_poll.h" }
-	files { "../3rdParty/mbedtls/library/ctr_drbg.c", "../3rdParty/mbedtls/include/mbedtls/ctr_drbg.h" }
-	files { "../3rdParty/mbedtls/library/md.c", "../3rdParty/mbedtls/include/mbedtls/md.h" }
-	files { "../3rdParty/mbedtls/library/md_wrap.c", "../3rdParty/mbedtls/include/mbedtls/md_wrap.h" }
-	files { "../3rdParty/mbedtls/library/asn1parse.c", "../3rdParty/mbedtls/include/mbedtls/asn1parse.h" }
-	files { "../3rdParty/mbedtls/library/oid.c", "../3rdParty/mbedtls/include/mbedtls/oid.h" }
-	files { "../3rdParty/mbedtls/library/timing.c", "../3rdParty/mbedtls/include/mbedtls/timing.h" }
-	files { "../3rdParty/mbedtls/library/dhm.c", "../3rdParty/mbedtls/include/mbedtls/dhm.h" }
 
 	files { "project.lua" }
 
@@ -29,6 +24,28 @@ project ("udPlatform" .. (projectSuffix or ""))
 
 	-- include common stuff
 	dofile "../common-proj.lua"
+	
+	filter { "options:mbedTLS=minimal" }
+		files { "../3rdParty/mbedtls/include/mbedtls/config.h" }
+		files { "../3rdParty/mbedtls/library/aes.c", "../3rdParty/mbedtls/include/mbedtls/aes.h" }
+		files { "../3rdParty/mbedtls/library/sha1.c", "../3rdParty/mbedtls/include/mbedtls/sha1.h" }
+		files { "../3rdParty/mbedtls/library/sha256.c", "../3rdParty/mbedtls/include/mbedtls/sha256.h" }
+		files { "../3rdParty/mbedtls/library/sha512.c", "../3rdParty/mbedtls/include/mbedtls/sha512.h" }
+		files { "../3rdParty/mbedtls/library/rsa.c", "../3rdParty/mbedtls/include/mbedtls/rsa.h" }
+		files { "../3rdParty/mbedtls/library/bignum.c", "../3rdParty/mbedtls/include/mbedtls/bignum.h" }
+		files { "../3rdParty/mbedtls/library/entropy.c", "../3rdParty/mbedtls/include/mbedtls/entropy.h" }
+		files { "../3rdParty/mbedtls/library/entropy_poll.c", "../3rdParty/mbedtls/include/mbedtls/entropy_poll.h" }
+		files { "../3rdParty/mbedtls/library/ctr_drbg.c", "../3rdParty/mbedtls/include/mbedtls/ctr_drbg.h" }
+		files { "../3rdParty/mbedtls/library/md.c", "../3rdParty/mbedtls/include/mbedtls/md.h" }
+		files { "../3rdParty/mbedtls/library/md_wrap.c", "../3rdParty/mbedtls/include/mbedtls/md_wrap.h" }
+		files { "../3rdParty/mbedtls/library/asn1parse.c", "../3rdParty/mbedtls/include/mbedtls/asn1parse.h" }
+		files { "../3rdParty/mbedtls/library/oid.c", "../3rdParty/mbedtls/include/mbedtls/oid.h" }
+		files { "../3rdParty/mbedtls/library/timing.c", "../3rdParty/mbedtls/include/mbedtls/timing.h" }
+		files { "../3rdParty/mbedtls/library/dhm.c", "../3rdParty/mbedtls/include/mbedtls/dhm.h" }
+		
+	filter { "options:mbedTLS=full" }
+		files { "../3rdParty/mbedtls/library/*.c", "../3rdParty/mbedtls/include/mbedtls/*.h" }
+		defines { "FULL_MBEDTLS" }
 
 	filter { "configurations:Release", "system:Windows" }
 		symbols "On"
