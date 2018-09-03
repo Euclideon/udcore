b9e9ac0d7847397e46f631d5d1182e5ed3eadf8d
Samuel Surtees <ssurtees@euclideon.com>
2018-09-03 12:34:26 +1000

    Improved memory leak tracking on Windows

--- udPlatform/Include/udNew.h
+++ udPlatform/Include/udNew.h
@@ -5,21 +5,21 @@
 #include "udPlatform.h"
 
 
-#define udNew(type, ...) new (_udAlloc(sizeof(type), udAF_None IF_MEMORY_DEBUG(__FILE__, __LINE__))) type(__VA_ARGS__)
-#define udNewNoParams(type) new (_udAlloc(sizeof(type), udAF_None IF_MEMORY_DEBUG(__FILE__, __LINE__))) type()
-#define udNewFlags(type, extra, flags, ...) new (_udAlloc(sizeof(type) + extra, flags IF_MEMORY_DEBUG(__FILE__, __LINE__))) type(__VA_ARGS__)
-#define udNewFlagsNoParams(type, extra, flags) new (_udAlloc(sizeof(type) + extra, flags IF_MEMORY_DEBUG(__FILE__, __LINE__))) type()
+#define udNew(type, ...) new (_udAlloc(sizeof(type), udAF_None, IF_MEMORY_DEBUG(__FILE__, __LINE__))) type(__VA_ARGS__)
+#define udNewNoParams(type) new (_udAlloc(sizeof(type), udAF_None, IF_MEMORY_DEBUG(__FILE__, __LINE__))) type()
+#define udNewFlags(type, extra, flags, ...) new (_udAlloc(sizeof(type) + extra, flags, IF_MEMORY_DEBUG(__FILE__, __LINE__))) type(__VA_ARGS__)
+#define udNewFlagsNoParams(type, extra, flags) new (_udAlloc(sizeof(type) + extra, flags, IF_MEMORY_DEBUG(__FILE__, __LINE__))) type()
 
 template <typename T>
-void _udDelete(T *&pMemory IF_MEMORY_DEBUG(const char * pFile = __FILE__, int  line = __LINE__))
+void _udDelete(T *&pMemory, const char *pFile, int line)
 {
   if (pMemory)
   {
     pMemory->~T();
-    _udFree(pMemory IF_MEMORY_DEBUG(pFile, line));
+    _udFree(pMemory, pFile, line);
   }
 }
-#define udDelete(pMemory) _udDelete(pMemory IF_MEMORY_DEBUG(__FILE__, __LINE__))
+#define udDelete(pMemory) _udDelete(pMemory, IF_MEMORY_DEBUG(__FILE__, __LINE__))
 
 
 #endif // UDNEW_H
