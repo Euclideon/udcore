5629f8226ec968b13bb2930aff548d050c7151ef
Paul Fox <pfox@euclideon.com>
2019-07-02 17:02:59 +1000

    Added udSafeDeque for a strict thread-safe deque API

---
+++ Include/udSafeDeque.h
@@ -0,0 +1,149 @@
+#ifndef UDSAFEDEQUE_H
+#define UDSAFEDEQUE_H
+//
+// Copyright (c) Euclideon Pty Ltd
+//
+// Creator: Paul Fox, November 2015 (Originally from cutil and later updated in vCore)
+//
+// Wraps udChunkedArray to provide a thread safe strict deque interface
+//
+
+#include "udResult.h"
+#include "udChunkedArray.h"
+#include "udThread.h"
+
+/// Thread safe double ended queue
+template <typename T>
+struct udSafeDeque
+{
+  udChunkedArray<T> chunkedArray;
+  udMutex *pMutex = nullptr;
+};
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+udResult udSafeDeque_Create(udSafeDeque<T> **ppDeque, size_t elementCount)
+{
+  udResult result = udR_Failure_;
+  udSafeDeque<T> *pDeque = nullptr;
+
+  UD_ERROR_NULL(ppDeque, udR_InvalidParameter_);
+  UD_ERROR_IF(elementCount == 0, udR_InvalidParameter_);
+
+  pDeque = udAllocType(udSafeDeque<T>, 1, udAF_Zero);
+
+  UD_ERROR_NULL(pDeque, udR_MemoryAllocationFailure);
+  UD_ERROR_CHECK(pDeque->chunkedArray.Init(elementCount));
+
+  pDeque->pMutex = udCreateMutex();
+  UD_ERROR_NULL(pDeque, udR_MemoryAllocationFailure);
+
+  *ppDeque = pDeque;
+  pDeque = nullptr;
+
+epilogue:
+  udSafeDeque_Destroy(&pDeque); // Will be nullptr on success
+
+  return result;
+}
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+void udSafeDeque_Destroy(udSafeDeque<T> **ppDeque)
+{
+  if (ppDeque == nullptr || *ppDeque == nullptr)
+    return;
+
+  udDestroyMutex(&(*ppDeque)->pMutex);
+  (*ppDeque)->chunkedArray.Deinit();
+
+  udFree(*ppDeque);
+}
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+inline udResult udSafeDeque_PushBack(udSafeDeque<T> *pDeque, const T &v)
+{
+  udResult result = udR_Failure_;
+  udMutex *pMutex = nullptr;
+
+  UD_ERROR_NULL(pDeque, udR_InvalidParameter_);
+
+  pMutex = udLockMutex(pDeque->pMutex);
+
+  UD_ERROR_NULL(pMutex, udR_NotInitialized_);
+  UD_ERROR_CHECK(pDeque->chunkedArray.PushBack(v));
+
+epilogue:
+  udReleaseMutex(pMutex);
+
+  return result;
+}
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+inline udResult udSafeDeque_PushFront(udSafeDeque<T> *pDeque, const T &v)
+{
+  udResult result = udR_Failure_;
+  udMutex *pMutex = nullptr;
+
+  UD_ERROR_NULL(pDeque, udR_InvalidParameter_);
+
+  pMutex = udLockMutex(pDeque->pMutex);
+
+  UD_ERROR_NULL(pMutex, udR_NotInitialized_);
+  UD_ERROR_CHECK(pDeque->chunkedArray.PushFront(v));
+
+epilogue:
+  udReleaseMutex(pMutex);
+
+  return result;
+}
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+inline udResult udSafeDeque_PopBack(udSafeDeque<T> *pDeque, T *pData)
+{
+  udResult result = udR_Success;
+  udMutex *pMutex = nullptr;
+
+  UD_ERROR_NULL(pDeque, udR_InvalidParameter_);
+
+  pMutex = udLockMutex(pDeque->pMutex);
+
+  UD_ERROR_NULL(pMutex, udR_NotInitialized_);
+  UD_ERROR_IF(!pDeque->chunkedArray.PopBack(pData), udR_ObjectNotFound);
+
+epilogue:
+  udReleaseMutex(pMutex);
+
+  return result;
+}
+
+// ****************************************************************************
+// Author: Paul Fox, November 2015
+template <typename T>
+inline udResult udSafeDeque_PopFront(udSafeDeque<T> *pDeque, T *pData)
+{
+  udResult result = udR_Success;
+  udMutex *pMutex = nullptr;
+
+  UD_ERROR_NULL(pDeque, udR_InvalidParameter_);
+
+  pMutex = udLockMutex(pDeque->pMutex);
+
+  UD_ERROR_NULL(pMutex, udR_NotInitialized_);
+  UD_ERROR_IF(!pDeque->chunkedArray.PopFront(pData), udR_ObjectNotFound);
+
+epilogue:
+  udReleaseMutex(pMutex);
+
+  return result;
+}
+
+#endif // UDSAFEDEQUE_H
---
+++ udTest/src/udSafeDequeTests.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include "udSafeDeque.h"
+
+TEST(udSafeDequeTests, ValidationTests)
+{
+  udSafeDeque<int> *pQueue = nullptr;
+  int result = -1;
+
+  EXPECT_EQ(udR_Success, udSafeDeque_Create(&pQueue, 32));
+  ASSERT_NE(nullptr, pQueue);
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PushBack(pQueue, 5));
+  EXPECT_EQ(udR_Success, udSafeDeque_PushFront(pQueue, 3));
+  EXPECT_EQ(udR_Success, udSafeDeque_PushBack(pQueue, 9));
+  EXPECT_EQ(udR_Success, udSafeDeque_PushBack(pQueue, -1));
+  EXPECT_EQ(udR_Success, udSafeDeque_PushFront(pQueue, 27));
+
+  // Queue should be { 27, 3, 5, 9, -1 }
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PopBack(pQueue, &result));
+  EXPECT_EQ(-1, result);
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PopFront(pQueue, &result));
+  EXPECT_EQ(27, result);
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PopFront(pQueue, &result));
+  EXPECT_EQ(3, result);
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PopFront(pQueue, &result));
+  EXPECT_EQ(5, result);
+
+  EXPECT_EQ(udR_Success, udSafeDeque_PopFront(pQueue, &result));
+  EXPECT_EQ(9, result);
+
+  EXPECT_EQ(udR_ObjectNotFound, udSafeDeque_PopFront(pQueue, &result));
+  EXPECT_EQ(udR_ObjectNotFound, udSafeDeque_PopBack(pQueue, &result));
+
+  EXPECT_EQ(9, result); // This should not have changed
+
+  udSafeDeque_Destroy(&pQueue);
+
+  EXPECT_EQ(nullptr, pQueue);
+}
