ec1a57c4dda7ffa5746b52214c72066b56f94bf0
Dave Pevreal <dpevreal@euclideon.com>
2023-07-31 16:49:24 +1000

    Split the udImageStreaming_SaveAs into two functions to access the full 24-bit image without having to save it to disk

--- udTest/src/udImageTests.cpp
+++ udTest/src/udImageTests.cpp
@@ -12,22 +12,22 @@ TEST(udImageTests, SaveLoad)
   uint32_t imageData[dim * dim];
   udImage testImage = { dim, dim, 3, 0, imageData };
   udImage *pReload = nullptr;
-  const char *pRawOutput = nullptr;
+  const char *pRawOutputFilename = nullptr;
   uint32_t sz;
 
   for (int i = 0; i < (dim * dim); ++i)
     imageData[i] = rand() | 0xff000000;
 
-  ASSERT_EQ(udR_Success, udFile_GenerateRawFilename(&pRawOutput, nullptr, 0, udCT_None, "testoutput", rawBufferSize));
+  ASSERT_EQ(udR_Success, udFile_GenerateRawFilename(&pRawOutputFilename, nullptr, 0, udCT_None, "testoutput", rawBufferSize));
 
   for (udImageSaveType saveType = (udImageSaveType)0; saveType < udIST_Max; saveType = (udImageSaveType)(saveType + 1))
   {
-    EXPECT_EQ(udR_InvalidParameter, udImage_Save(nullptr, pRawOutput, nullptr, saveType));
+    EXPECT_EQ(udR_InvalidParameter, udImage_Save(nullptr, pRawOutputFilename, nullptr, saveType));
     EXPECT_EQ(udR_InvalidParameter, udImage_Save(&testImage, nullptr, nullptr, saveType));
-    EXPECT_EQ(udR_InvalidParameter, udImage_Save(&testImage, pRawOutput, nullptr, (udImageSaveType)(udIST_Max + saveType)));
+    EXPECT_EQ(udR_InvalidParameter, udImage_Save(&testImage, pRawOutputFilename, nullptr, (udImageSaveType)(udIST_Max + saveType)));
     // Test with a null for the size for just the first type
-    ASSERT_EQ(udR_Success, udImage_Save(&testImage, pRawOutput, &sz, saveType));
-    EXPECT_EQ(udR_Success, udImage_Load(&pReload, pRawOutput));
+    ASSERT_EQ(udR_Success, udImage_Save(&testImage, pRawOutputFilename, &sz, saveType));
+    EXPECT_EQ(udR_Success, udImage_Load(&pReload, pRawOutputFilename));
     if (pReload)
     {
       EXPECT_EQ(testImage.width, pReload->width);
@@ -41,5 +41,48 @@ TEST(udImageTests, SaveLoad)
       udImage_Destroy(&pReload);
     }
   }
-  udFree(pRawOutput);
+  udFree(pRawOutputFilename);
+}
+
+TEST(udImageTests, SaveLoadImageStreaming)
+{
+  enum
+  {
+    dim = 32,
+  };
+  uint32_t imageData[dim * dim];
+  udImage testImage = { dim, dim, 3, 0, imageData };
+  udImageStreaming *pReload = nullptr;
+  udImageStreamingOnDisk *pOnDisk = nullptr;
+  const char *pRawOutputFilename = nullptr;
+  uint32_t sz;
+  udFile *pLoadFile = nullptr;
+  uint8_t *pRGB = nullptr;
+
+  for (int i = 0; i < (dim * dim); ++i)
+    imageData[i] = rand() | 0xff000000;
+
+  // Test invalid parameters
+  EXPECT_EQ(udR_InvalidParameter, udImageStreaming_Save(nullptr, &pOnDisk, nullptr, nullptr));
+  EXPECT_EQ(udR_InvalidParameter, udImageStreaming_Save(&testImage, nullptr, nullptr, nullptr));
+
+  ASSERT_EQ(udR_Success, udImageStreaming_Save(&testImage, &pOnDisk, &sz, "testdesc"));
+  ASSERT_EQ(udR_Success, udFile_GenerateRawFilename(&pRawOutputFilename, pOnDisk, sz, udCT_None, "testoutput"));
+  udFree(pOnDisk);
+
+  ASSERT_EQ(udR_Success, udFile_Open(&pLoadFile, pRawOutputFilename, udFOF_Read));
+  EXPECT_EQ(udR_Success, udImageStreaming_Load(&pReload, pLoadFile, 0));
+  if (pReload)
+  {
+    EXPECT_EQ(testImage.width, pReload->width);
+    EXPECT_EQ(testImage.height, pReload->height);
+    EXPECT_EQ(udR_Success, udImageStreaming_GetImage24(pReload, &pRGB, udISF_TopLeft | udISF_ABGR));
+    // Test each pixel as 24-bit
+    for (int i = dim * dim - 1; i >= 0; --i)
+      EXPECT_EQ(0, memcmp(testImage.pImageData + i, pRGB + (i * 3), 3));
+    udImageStreaming_Destroy(&pReload);
+  }
+  udFile_Close(&pLoadFile);
+  udFree(pRGB);
+  udFree(pRawOutputFilename);
 }
